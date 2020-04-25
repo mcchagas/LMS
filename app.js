609f30be40f70148020e73970e325b8604115a82
MCH Devops <chagas.marcelo@outlook.com>
2020-04-25 23:01:09 +0400

    All  Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const loginRouter = require("./routes/loginRoute");
+const registerRouter = require("./routes/registerRoute");
+const courseTypeRouter = require("./routes/courseTypeRoute");
+const trainingTypeRouter = require("./routes/trainingtypeRoute");
+const courseCategoryRouter = require("./routes/courseCategoryRoute");
+const courseStatusRouter = require("./routes/courseStatusRoute");
+const courseAreaRouter = require("./routes/courseAreaRoute");
+
+
+app.use(express.json());
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/courseType', courseTypeRouter);
+app.use('/trainingType', trainingTypeRouter);
+app.use('/courseCategory', courseCategoryRouter);
+app.use('/courseStatus', courseStatusRouter);
+app.use('/courseArea', courseAreaRouter);                 // it will be business, HR, Administration,...
+app.use(express.static(`${__dirname}/public`));
+
+
+//middleware capture current date
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+  });
+  
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config({path: './config.env'});
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DB_LMS_PASSWORD);
+mongoose.connect(DB, {
+  useNewUrlParser: true, 
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => {
+//  console.log(con.connections);
+  console.log('DB Connection to LMS collection in MongoDB successful');
+})
+
+
+
+//server running na porta 3000 - START SERVER
+const port = "3000";
+app.listen(port, () => {
+  console.log(`The app LMS is running on the port ${port}`);
+});
