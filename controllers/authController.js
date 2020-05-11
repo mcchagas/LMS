084556eab98bc20f704765d3cd3c7bb757e3f889
MCH Devops <chagas.marcelo@outlook.com>
2020-05-11 20:44:06 -0300

    May 110  2020.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,15 @@ const User = require('./../models/userModel');
 
 exports.signup = async (req, res, next) =>{
 try {
-        const newUser = await User.create(req.body);
+        //const newUser = await User.create(req.body); anterior sem seguranca / abaixo o novo com os campos.
+        const newUser = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            passwordConfirm: req.body.passwordConfirm,
+            mobile: req.body.mobile
+        });
+
         res.status(201).json({
             status: 'success',
             data: {
